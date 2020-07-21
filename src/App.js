01d81360db4ba4a44275700392e85003cf040968
dragon <gildydtjd@naver.com>
2020-07-21 16:52:50 +0900

    scrollMagic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import List from './javascript/magic/List';
+import ClassToggle from './javascript/magic/ClassToggle';
 function App() {
   return (
     <div className="App">
-      <List />
+      <ClassToggle />
     </div>
   );
 }
